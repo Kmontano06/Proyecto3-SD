8bfaf591ac7ccd0dc4873df195641778ab2967c4
Kmontano06 <keniamontano06@gmail.com>
2022-11-26 14:40:56 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
 import './assets/css/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
+
 const app = createApp(App);
   
 app.config.globalProperties.url = 'https://proyecto3-comicsapp-redis.netlify.app';
+
 app.use(router).mount("#app");
