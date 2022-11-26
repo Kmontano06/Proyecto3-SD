f91d857aed0d0109fe91a15183256f34194a07ee
Kmontano06 <keniamontano06@gmail.com>
2022-11-26 13:43:10 -0600

    Update ComicIndex.vue

--- src/components/ComicIndex.vue
+++ src/components/ComicIndex.vue
@@ -22,10 +22,10 @@
       <tbody class="scroll-area" v-if='comics.length != 0'>
         <tr v-for='comic in comics'>
           <td><img v-bind:src="comic.img"  width="80" height="110"></td>
-          <td>  <router-link :to="'/comic/edit/'+comic._id">{{comic.title}}</router-link></td>
+          <td>  <router-link :to="'/comic/edit/'+comic.id">{{comic.title}}</router-link></td>
           <td><!--<router-link :to="'/personaje/edit/'+comic.personaje.personaje_id">{{comic.personaje.name}}</router-link>--></td>
           <td><!--<router-link :to="'/ilustrador/edit/'+comic.ilustrador.ilustrador_id">{{comic.ilustrador.name}}</router-link>--></td>
-          <td><button class="btn btn-danger" v-on:click="deleteComic(comic._id)"><img src="../assets/images/eliminar.png" alt="" width="15"/></button></td>
+          <td><button class="btn btn-danger" v-on:click="deleteComic(comic.id)"><img src="../assets/images/eliminar.png" alt="" width="15"/></button></td>
         </tr>
       </tbody>
        <tbody class="scroll-area" v-if='comics.length == 0'>
